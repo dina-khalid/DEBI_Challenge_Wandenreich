9e4d8a99c388454ed35adad54eb6cba4ae8951d2
omniaSayed <somnia060@gmail.com>
2023-04-08 15:23:21 +0200

    connecting debi world to catkin workspace

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/rama2/catkin_ws/src
+# Build directory: /home/rama2/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("turtlebot3_debipkg")
---
+++ build/turtlebot3_debipkg/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/rama2/catkin_ws/src/turtlebot3_debipkg
+# Build directory: /home/rama2/catkin_ws/build/turtlebot3_debipkg
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
