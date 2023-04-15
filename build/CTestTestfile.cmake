56a61034b0748aa4fb4078511b41ec118d0e39cb
Dina Khalid <69371669+dina-khalid@users.noreply.github.com>
2023-04-15 15:08:18 +0200

    Delete build directory

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/rama2/catkin_ws/src
-# Build directory: /home/rama2/catkin_ws/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("turtlebot3_debipkg")
